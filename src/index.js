60c08d65fe910a7100c8718eb2d53055b4d17fb2
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-14 22:11:13 +0300

    added firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -6,6 +9,15 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./store/store";
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDXfTZ3psriCjUg4KJR2oPdYauc6Fg907k",
+  authDomain: "aston-books-app.firebaseapp.com",
+  projectId: "aston-books-app",
+  storageBucket: "aston-books-app.appspot.com",
+  messagingSenderId: "291102209326",
+  appId: "1:291102209326:web:37291ee8a7f0a4e80af6bf",
+  measurementId: "G-CPQ75CS8LV",
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
