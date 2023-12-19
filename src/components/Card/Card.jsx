b373a0e6a611b5dcd679785d75203f7bd61740b4
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-19 16:17:32 +0300

    misc changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { Link, useLocation } from "react-router-dom";
 import fake from "../../images/fakeCover.png";
 import classes from "./Card.module.scss";
 
-function Card({ author, title, cover }) {
+function Card({ id, author, title, cover }) {
+  const location = useLocation();
+
   return (
     <div className={classes.card}>
       <img className={classes.top_image} src={cover || fake} alt="cover" />
@@ -17,9 +20,13 @@ function Card({ author, title, cover }) {
           <div className={classes.add_btn}>
             <span className={classes.inner_font}>В избранное</span>
           </div>
-          <div className={classes.about}>
+          <Link
+            className={classes.about}
+            to={`/${id}`}
+            state={{ from: location }}
+          >
             <span className={classes.inner_font}>Подробнее</span>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
