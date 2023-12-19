43e239a673a1615794597d5121185c3ec529693d
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-19 16:23:58 +0300

    added about page route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../components/MainPage/MainPage";
+import About from "../pages/About";
 
 function AppRouter() {
   return (
     <div>
       <Routes>
         <Route path="/*" element={<MainPage />} />
+        <Route path="/:id" element={<About />} />
       </Routes>
     </div>
   );
