368d839b9a7fe3d0bf854b27d8281f83dc01af37
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-21 20:31:19 +0300

    added links to pages in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,65 @@
-import { Button } from "@mui/base";
-import logo from "../../images/logo.svg";
-import "./style.scss";
+import classNames from "classnames";
 
-function Header() {
+import { NavLink } from "react-router-dom";
+import logo from "../../images/logo.svg";
+import classes from "./header.module.scss";
+
+function Header({ isAuth, logout }) {
+  const isLinkActive = ({ isActive }) =>
+    classNames(classes.book_link, {
+      [classes.book_link_active]: isActive,
+    });
+  const exitApp = () => {
+    logout();
+  };
   return (
-    <div className="main-header-container">
-      <div className="navigation-bar">
-        <img className="logo" src={logo} alt="logo" />
-        <p className="book-link">Все книги</p>
-        <p className="book-link">Избранное</p>
-        <p className="book-link">История</p>
+    <div className={classes.main_header_container}>
+      <div className={classes.navigation_bar}>
+        {isAuth ? (
+          <>
+            <img className={classes.logo} src={logo} alt="logo" />
+            <NavLink to="/" className={isLinkActive}>
+              Все книги
+            </NavLink>
+            <NavLink to="/favs" className={isLinkActive}>
+              Избранное
+            </NavLink>
+            <NavLink to="/history" className={isLinkActive}>
+              История
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <img className={classes.logo} src={logo} alt="logo" />
+            <NavLink to="/" className={isLinkActive}>
+              Все книги
+            </NavLink>
+            <NavLink to="/signin" className={classes.book_link}>
+              Избранное
+            </NavLink>
+            <NavLink to="/signin" className={classes.book_link}>
+              История
+            </NavLink>
+          </>
+        )}
       </div>
-      <div className="login-section">
-        <Button className="login-button">Войти</Button>
-      </div>
+      {isAuth ? (
+        <div className={classes.login_section}>
+          <button
+            type="button"
+            className={classes.login_button}
+            onClick={exitApp}
+          >
+            Выйти
+          </button>
+        </div>
+      ) : (
+        <div className={classes.login_section}>
+          <NavLink to="/signin" className={classes.login_button}>
+            Войти
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 }
