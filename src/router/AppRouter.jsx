67b457efd06682f98740fb86c8ede4f4c72d33df
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-23 16:27:44 +0300

    added private and public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import MainPage from "../components/MainPage/MainPage";
-import About from "../pages/About";
+import { useAuth } from "../hooks/useAuth";
+import { PrivateRoutes } from "./PrivateRoutes";
+
+const MainPage = lazy(() => import("../pages/MainPage/MainPage"));
+const About = lazy(() => import("../pages/About/About"));
+const Favorites = lazy(() => import("../pages/Favorites/Favorites"));
+const History = lazy(() => import("../pages/History/History"));
+const Login = lazy(() => import("../pages/Login/Login"));
+const Registration = lazy(
+  () => import("../pages/Registration/RegistrationPage")
+);
+const Searches = lazy(() => import("../pages/Searches/Searches"));
+const AppLayout = lazy(() => import("../pages/AppLayout/AppLayout"));
+const Page404 = lazy(() => import("../pages/Page404/Page404"));
 
 function AppRouter() {
+  const { isAuth } = useAuth();
   return (
-    <div>
-      <Routes>
-        <Route path="/*" element={<MainPage />} />
-        <Route path="/:id" element={<About />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path=":id" element={<About />} />
+        <Route path="signin" element={<Login />} />
+        <Route path="signup" element={<Registration />} />
+        <Route path="search" element={<Searches />} />
+        <Route path="*" element={<Page404 />} />
+        <Route element={<PrivateRoutes isAuth={isAuth} />}>
+          <Route path="favs" element={<Favorites />} />
+          <Route path="history" element={<History />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
