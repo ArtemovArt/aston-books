2d7f9a796e0c221bcdcf8af9d6e7a0d6d8112e2d
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-24 00:54:45 +0300

    added redirect hook usage

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -5,11 +5,15 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Loader from "../../components/Loader/Loader";
 import { useAuth } from "../../hooks/useAuth";
+import useRedirect from "../../hooks/useRedirect";
+import { isLoading } from "../../store/selectors/userSelector";
 import "./AppLayout.scss";
 
 export default function AppLayout() {
   const { isAuth, logout } = useAuth();
-  const isUserLoading = useSelector((state) => state.userReducer.isLoading);
+  const isUserLoading = useSelector(isLoading);
+
+  useRedirect();
 
   return (
     <div className="layout">
